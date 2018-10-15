30ae0af1f5f9965f3bd8149948e1180b281e2972
Jagoda <hryszko.jagoda13@gmail.com>
2018-10-15 22:05:14 +0200

    flex design

--- cookie.js
+++ cookie.js
@@ -1,7 +1,13 @@
-
+var clickNum =0;
 function coockieClick() {
-	var clickNum =0;
-	var clickNum = clickNum + 1;
-	alert(clickNum);
-
+	clickNum = clickNum + 1;
+	var cookieCounter = document.getElementById("cookieCounter");
+	cookieCounter.innerHTML = clickNum;
+	var image = document.getElementById("cookieImage");
+	image.classList.add("resize");
+	setTimeout(removeClass, 100);
+}
+function removeClass() {
+	var image = document.getElementById("cookieImage");
+	image.classList.remove("resize");
 }
